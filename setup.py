f66403b4663eb352f3623daa8c0d7f116c30bbe3
Manuel Kaufmann <humitos@gmail.com>
2018-05-23 18:10:57 -0500

    Add simple setup.py file

---
+++ setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+import setuptools
+
+setuptools.setup(
+    name='sphinx-version-warning',
+    version='0.0.1',
+    author='Manuel Kaufmann',
+    author_email='humitos@gmail.com',
+    description='Sphinx extension to add a warning banner',
+    url='https://github.com/humitos/sphinx-version-warning',
+    packages=setuptools.find_packages(),
+    classifiers=(
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ),
+    install_requires=[
+        'munch',
+        'slumber',
+    ],
+)
